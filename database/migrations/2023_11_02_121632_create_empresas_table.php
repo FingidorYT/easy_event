663e9cc65d16041091f4c0400cb40e53ce91ab5b
Naren Flórez <64817703+NarenFlopi@users.noreply.github.com>
2023-11-02 10:14:57 -0500

    Tablas productos, empresa y categoria

--- database/migrations/2023_11_02_121632_create_empresas_table.php
+++ database/migrations/2023_11_02_121632_create_empresas_table.php
@@ -12,12 +12,20 @@ class CreateEmpresasTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('empresas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('empresas', function (Blueprint $table) {
+        $table->id();
+        $table->bigInteger('nit_empresa');
+        $table->string('direccion_empresa');
+        $table->string('nombre_empresa');
+        $table->bigInteger('telefono_empresa');
+        $table->string('email_empresa');
+        $table->bigInteger('users_id'); 
+        $table->foreign('users_id')->references('id')->on('users');
+         //$table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
