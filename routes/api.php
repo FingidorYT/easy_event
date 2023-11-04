3dad4925e107599a56cce08789b69a951d7ed67d
Naren Flórez <64817703+NarenFlopi@users.noreply.github.com>
2023-11-03 23:02:29 -0500

    CRUD de Categorias

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EmpresaApiController;
+use App\Http\Controllers\API\CategoriaApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,8 +32,10 @@ Route::group([
       'middleware' => 'auth:api'
     ], function() {
         Route::get('logout', [AuthController::class,'logout']);
+        Route::apiResource('categorias', CategoriaApiController::class);
+        Route::apiResource('empresas', EmpresaApiController::class);
         Route::get('user', [AuthController::class,'user']);
     });
 });
 
-Route::apiResource('empresas', EmpresaApiController::class);
+
